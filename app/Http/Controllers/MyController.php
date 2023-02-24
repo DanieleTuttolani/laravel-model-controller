b047e67cf7ba071e90c4c07700ee6289565845e6
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-02-24 15:43:14 +0100

    ticket

---
+++ app/Http/Controllers/MyController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use  App\Models\Movie;
+use Illuminate\Http\Request;
+
+class MyController extends Controller
+{
+    public function Film()
+    {
+        $movies = Movie::all();
+        
+        return view('prova' , compact('movies'));
+    }
+}
